d1a60711f4bb199aa88d65c3b96afddf19d565fa
Subodh106 <subodhloves106@gmail.com>
2026-05-07 11:37:25 +0545

    create group is in progress

--- components/web/CreateGroup.tsx
+++ components/web/CreateGroup.tsx
@@ -1,12 +1,59 @@
-import { DialogTitle } from "../ui/dialog"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
 
+import { PlusIcon } from "lucide-react";
+import { Field, FieldGroup } from "../ui/field";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 const CreateGroup = () => {
   return (
-    <DialogTitle>
-      hello world
-    </DialogTitle>
-  )
-}
+    <Dialog>
+      <form>
+      <DialogTrigger asChild>
+        <Button className="cursor-pointer hover:bg-green-500 bg-green-600">
+          
+          <PlusIcon /> Create Group
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-sm">
+        <DialogHeader>
+          <DialogTitle>Create Group</DialogTitle>
+          <DialogDescription>
+              Create your own group
+          </DialogDescription>
+        </DialogHeader>
+        <FieldGroup>
+          <Field>
+            <Label htmlFor="group-name">Group Name</Label>
+          <Input
+          id="group-name"
+          name="group-name"
+          autoCorrect="group-name"
+          />
+          </Field>
+        </FieldGroup>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant={"outline"}>Cancel</Button>
+          </DialogClose>
+          <Button>
+            <PlusIcon></PlusIcon>
+            Create Group</Button>
+        </DialogFooter>
+      </DialogContent>
+      </form>
+    </Dialog>
+  );
+};
 
-export default CreateGroup
+export default CreateGroup;
