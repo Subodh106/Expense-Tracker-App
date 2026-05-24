df946e606eddc3a92253db4549b32dae968d6965
Subodh106 <subodhloves106@gmail.com>
2026-05-24 21:00:49 +0545

    refactor: replace NextResponse with ApiError for error handling in invite rejection route

--- app/api/invite/[inviteId]/reject/route.ts
+++ app/api/invite/[inviteId]/reject/route.ts
@@ -1,11 +1,12 @@
 import connectdb from "@/db/connectDb";
+import { ApiError } from "@/helpers/apiError";
 import { apiResponse } from "@/helpers/apiresponse";
 import { getInfo } from "@/helpers/getinfo";
 import { Group } from "@/models/Group.model";
 import { GroupInvite } from "@/models/GroupInvites.model";
 import { User } from "@/models/User.model";
 import { Types } from "mongoose";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest} from "next/server";
 
 export async function PATCH(req:NextRequest,{params}:{params:Promise<{inviteId:string}>}) {
     try {
@@ -13,43 +14,42 @@ export async function PATCH(req:NextRequest,{params}:{params:Promise<{inviteId:s
         const { inviteId } = await params;
         const id = await getInfo();
         if(!id){
-            return NextResponse.json({message:"Unauthorized access"},{status:401});
+            throw new ApiError(401,"Unauthorized access");
         }
         if(!Types.ObjectId.isValid(id?.toString())){
-            return NextResponse.json({message:"Invalid id formate"},{status:400});
+            throw new ApiError(400,"Invalid id formate");
         }
         const isUserExist = await User.findById(new Types.ObjectId(id?.toString()));
         if(!isUserExist){
-            return NextResponse.json({message:"User doesn't exist"},{status:404})
+            throw new ApiError(404,"User doesn't exist");
         }
         if(!Types.ObjectId.isValid(inviteId?.toString())){
-            return NextResponse.json({message:"Invalid id formate"},{status:400});
+            throw new ApiError(400,"Invalid id formate");
         };
         const isInviteExist = await GroupInvite.findById(new Types.ObjectId(inviteId?.toString()));
         if(!isInviteExist){
-            return NextResponse.json({message:"Invite doens't exsit"},{status:404});
+            throw new ApiError(404,"Invite doesn't exist");
         }
         if(!Types.ObjectId.isValid(isInviteExist.group_id?.toString())){
-            return NextResponse.json({message:"Invalid id formate"},{status:400});
+            throw new ApiError(400,"Invalid id formate");
         }
         const isUserAllowedToAccept = isInviteExist.invitedUser_id?.toString()===id;
         if(!isUserAllowedToAccept){
-            return NextResponse.json({message:"User isn't allowed to accept the invite"},{status:401});
+            throw new ApiError(401,"User isn't allowed to accept the invites")
         }
         const isGroupExist = await Group.findById(new Types.ObjectId(isInviteExist.group_id?.toString()));
         if(!isGroupExist){
-            return NextResponse.json({message:"Group doesn't exist"},{status:404});
+            throw new ApiError(404,"Group doesn't exist");
         }
         const isInviteAlreadyAccepted = isInviteExist.status=="accepted";
         if(isInviteAlreadyAccepted){
-            return NextResponse.json({message:"Invited is already accepted"},{status:409})
+            throw new ApiError(409,"Invite is already accepted");
         }
         isInviteExist.status="rejected";
         isInviteExist.save();
-        // return NextResponse.json({message:"Rejected the request successfully"},{status:200});
         return new apiResponse(200,"Invited rejected successfully");
     } catch (error:any) {
         console.log("Error during accepting the invites:",error.message);
-        return NextResponse.json({message:"Error during rejecting the invites",error:error.message},{status:500});
+        throw new ApiError(500,"Error during rejecting the invites",error.message);
     }
 }
